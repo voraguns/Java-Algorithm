60404e0c7eef5e0b43deaebdc31addbb524a4d99
Voragun <voragun2543@gmail.com>
2023-04-18 21:12:57 +0700

    [Functional Programming] print reverse

--- code/Start.java
+++ code/Start.java
@@ -8,12 +8,20 @@ class Start {
         
         first.next.next = new Station();
         first.next.next.name = "Talingchan";
-        print(first);
+        //print(first);
+        show(first);
     }
     static void print(Station current) {
         if (current == null) return;
         System.out.println(current.name);
         print(current.next);
+    }
+    
+    static void show(Station current) {
+        if (current == null) return;
+        show(current.next);
+        System.out.println(current.name);
+        
     }
 }
 
