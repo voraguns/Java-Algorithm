2a9e3594639a601a273e65b63eda03eedcabd1f0
Voragun <voragun2543@gmail.com>
2023-04-18 20:22:24 +0700

    [Linked list] connect node ver.2

--- code/Start.java
+++ code/Start.java
@@ -2,14 +2,16 @@ class Start {
     public static void main(String[] args) {
         Station first = new Station();
         first.name = "Bang Sue";
-        System.out.println(first.name);
         
-        Station second = new Station();
-        second.name = "Bang Plad";
+        first.next = new Station();
+        first.next.name = "Bang Plad";
         
-        first.next = second;
+        first.next.next = new Station();
+        first.next.next.name = "Talingchan";
+        
         System.out.println(first.name);
         System.out.println(first.next.name);
+        System.out.println(first.next.next.name);
     }
 }
 
