d72e96574440c92b7e4d0a5f47da8e433df7cb7a
Voragun <voragun2543@gmail.com>
2023-04-18 20:50:29 +0700

    sample before functional programming

--- code/Start.java
+++ code/Start.java
@@ -1,24 +1,12 @@
 class Start {
     public static void main(String[] args) {
-        Station first = new Station();
-        first.name = "Bang Sue";
-        
-        first.next = new Station();
-        first.next.name = "Bang Plad";
-        
-        first.next.next = new Station();
-        first.next.next.name = "Talingchan";
-        
-        Station current = first;
-        for(Station c = first; c != null; c = c.next) {
-            System.out.println(c.name);
+        print(7);
+    }
+    static void print(int n) {
+        int i = 1;
+        while(i <= n) {
+            System.out.println(i);
+            i++;
         }
     }
-}
-
-class Station {
-    String name;
-    double area;
-    int staff;
-    Station next;
 }
