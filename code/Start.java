582bf419e71e8b9db225426a5b4669519cdec34e
Voragun <voragun2543@gmail.com>
2023-04-18 21:02:51 +0700

    [Functional Programming] print linked list by using function programming

--- code/Start.java
+++ code/Start.java
@@ -1,10 +1,23 @@
 class Start {
     public static void main(String[] args) {
-        print(7);
+        Station first = new Station();
+        first.name = "Bang Sue";
+        
+        first.next = new Station();
+        first.next.name = "Bang Plad";
+        
+        first.next.next = new Station();
+        first.next.next.name = "Talingchan";
+        print(first);
     }
-    static void print(int n) {
-        if(n == 0) return;
-        print(n-1);
-        System.out.println(n);
+    static void print(Station current) {
+        if (current == null) return;
+        System.out.println(current.name);
+        print(current.next);
     }
+}
+
+class Station {
+    String name;
+    Station next;
 }
