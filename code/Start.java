6c0a7878c29d593ef1de9388463f2c2ea9efab54
Voragun <voragun2543@gmail.com>
2023-04-19 20:00:55 +0700

    [binary tree] sample binary tree

--- code/Start.java
+++ code/Start.java
@@ -1,31 +1,60 @@
 class Start {
-    public static void main(String[] args) {
-        Station first = new Station();
-        first.name = "Bang Sue";
+    public static void main(String[] data) {
+        Player root;
+        root = new Player();
+        root.name = "Satoshi";
+        root.number = 7;
+        Player p = new Player();
+        p.name = "Frank";
+        p.number = 8;
         
-        first.next = new Station();
-        first.next.name = "Bang Plad";
+        root.left = p;
+        root.right = new Player();
+        root.right.name = "Michael";
+        root.right.number = 10;
         
-        first.next.next = new Station();
-        first.next.next.name = "Talingchan";
-        //print(first);
-        show(first);
-    }
-    static void print(Station current) {
-        if (current == null) return;
-        System.out.println(current.name);
-        print(current.next);
+        Player q = new Player();
+        root.left.left = q;
+        q.name = "Jordan";
+        q.number = 1;
+        
+        Player r = new Player();
+        root.right.left = r;
+        r.name = "James";
+        r.number = 9;
+        
+        System.out.println(root.number);
+        System.out.println(root.left.number);
+        System.out.println(root.right.number);
+        System.out.println(q.number);
+        System.out.println(r.number);
+        
+        int t = count(root);
+        int ff = sum(root);
+        System.out.println("Count node : " + t);
+        System.out.println("Sum node : " + ff);
     }
     
-    static void show(Station current) {
-        if (current == null) return;
-        show(current.next);
-        System.out.println(current.name);
-        
+    // Count the node in binary tree
+    static int count(Player x) {
+        if (x == null) return 0;
+        int a = count(x.left);
+        int b = count(x.right);
+        return a + b + 1;
+    }
+    
+    static int sum(Player x) {
+        if (x == null) return 0;
+        int a = sum(x.left);
+        int b = sum(x.right);
+        int c = x.number;
+        return a + b + c;
     }
 }
 
-class Station {
+class Player {
     String name;
-    Station next;
+    int number;
+    Player left;
+    Player right;
 }
