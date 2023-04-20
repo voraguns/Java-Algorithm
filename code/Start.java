59720e060d3cbe09875566dab93cf2ed1bbdbeff
Voragun <voragun2543@gmail.com>
2023-04-20 21:24:22 +0700

    breadth first search2

--- code/Start.java
+++ code/Start.java
@@ -1,14 +1,35 @@
 class Start {
     public static void main(String[] data) {
-        play("");
-    }
-    static void play(String current) {
-        if (current.length() == 5) {
-            System.out.println(current);
-            return;
+        int n = 0;
+        Queue q = new Queue();
+        q.create(1000);
+        q.insert("");
+        while(true) {
+            String current = q.remove();
+            if (current == null) break;
+            if (current.length() == 5) {
+                n++;
+                System.out.println(n + ": " + current);
+            } else {
+                q.insert(current + "W");
+                q.insert(current + "D");
+                q.insert(current + "L");
+            }
         }
-        play(current + "W");
-        play(current + "D");
-        play(current + "L");
+    }
+}
+
+class Queue {
+    String[] data;
+    int front = 0;
+    int rear = 0;
+    void create(int size) {
+        data = new String[size];
+    }
+    void insert(String e) {
+        data[rear++] = e;
+    }
+    String remove() {
+        return data[front++];
     }
 }
